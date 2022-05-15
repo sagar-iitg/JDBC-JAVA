a595950cd71a13c08005bc94152123ac94e0a24a
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2022-05-15 15:34:43 +0530

    added display image gui

---
+++ JDBC-NB/src/com/jdbc/practice/Helper.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jdbc.practice;
+
+import java.awt.Image;
+import java.io.InputStream;
+import java.sql.*;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+
+
+public class Helper {
+ 
+    
+    
+   public static ImageIcon getImageIconById(int id,Connection con){
+       ImageIcon icon=null;
+       
+       try{
+           
+           String q="select pic from images where id=?";
+           
+           PreparedStatement pstmt=con.prepareStatement(q);
+           pstmt.setInt(1,id);
+           ResultSet set=pstmt.executeQuery();
+           
+           if(set.next()){
+               Blob b=set.getBlob("pic");
+               InputStream is=b.getBinaryStream();
+               Image image=ImageIO.read(is);
+                icon=new ImageIcon(image);
+               
+           }
+           
+       }catch(Exception e){
+           e.printStackTrace();
+       }
+       return icon;
+       
+   }
+}
